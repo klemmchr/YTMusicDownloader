1acd23991d5dd52929d79a73d324cd3e424b2e14
Christian Klemm <chris579@web.de>
2016-11-11 20:31:14 +0100

    Removed: GitHub reported, changed to email only

--- src/YTMusicDownloaderAPI/Model/MailReporter.cs
+++ src/YTMusicDownloaderAPI/Model/MailReporter.cs
@@ -37,58 +37,43 @@ namespace YTMusicDownloaderAPI.Model
         }
 
         
-        public static bool SendMail(string ip, CrashReport report, int reportId)
+        public static bool SendMail(string ip, CrashReport report)
         {
             var sb = new StringBuilder();
             
-            if (reportId != -1)
-            {
-                sb.AppendLine("This is your automated crash report notifier.");
-                sb.AppendLine();
-                sb.Append("Used ip: ");
-                sb.AppendLine(ip);
-                sb.AppendLine();
-                sb.AppendLine("A new crash report for the YT Music downloader was created in GitHub.");
-                sb.AppendLine();
-                sb.Append($"You can find the issue directly under https://github.com/chris579/YTMusicDownloader/issues/");
-                sb.Append(reportId);
-            }
-            else
-            {
-                sb.AppendLine("Automated crash reporter for app YTMusicDownloader");
-                sb.AppendLine();
-                sb.AppendLine("The app crashed unexpectly! An issue in GitHub could not be created. Therefore the relevant information is passed here.");
-                sb.AppendLine();
-                sb.Append("IP: ");
-                sb.AppendLine(ip);
-                sb.AppendLine();
-                sb.Append("Time: ");
-                sb.AppendLine(report.Time);
-                sb.Append("Assembly version: ");
-                sb.AppendLine(report.AssemblyVersion);
-                sb.Append("Memory allocated: ");
-                sb.AppendLine(report.MemoryAllocated.ToString());
-                sb.Append("GUID: ");
-                sb.AppendLine(report.Guid);
-                sb.Append("Machine Name: ");
-                sb.AppendLine(report.MachineName);
-                sb.AppendLine();
-                sb.AppendLine("Exception trace");
-                sb.AppendLine("===================================================================================================");
-                sb.AppendLine();
-                sb.Append(report.Exception);
-            }
+            sb.AppendLine("Automated crash reporter for app YTMusicDownloader");
+            sb.AppendLine();
+            sb.AppendLine("The app crashed unexpectly! An issue in GitHub could not be created. Therefore the relevant information is passed here.");
+            sb.AppendLine();
+            sb.Append("IP: ");
+            sb.AppendLine(ip);
+            sb.AppendLine();
+            sb.Append("Time: ");
+            sb.AppendLine(report.Time);
+            sb.Append("Assembly version: ");
+            sb.AppendLine(report.AssemblyVersion);
+            sb.Append("Memory allocated: ");
+            sb.AppendLine(report.MemoryAllocated.ToString());
+            sb.Append("GUID: ");
+            sb.AppendLine(report.Guid);
+            sb.Append("Machine Name: ");
+            sb.AppendLine(report.MachineName);
+            sb.AppendLine();
+            sb.AppendLine("Exception trace");
+            sb.AppendLine("===================================================================================================");
+            sb.AppendLine();
+            sb.Append(report.Exception);
 
             try
             {
                 var mail = new MailMessage
                 {
-                    From = new MailAddress("chris579@web.de"),
+                    From = new MailAddress(Properties.Settings.EmailAddress),
                     Subject = "YT Downloader Crash Report",
                     Body = sb.ToString()
                 };
 
-                mail.To.Add("chris579@web.de");
+                mail.To.Add(Properties.Settings.EmailAddress);
 
                 Server.Send(mail);
                 return true;
